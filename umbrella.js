822a9316f95bedaf3c93cd8b377d31fb4a445e11
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2021-01-21 16:59:18 +0530

    Update umbrella.js

--- umbrella.js
+++ umbrella.js
@@ -1,13 +1,14 @@
 class Umbrella{
-    constructor(x,y,radius){
-        var op={
+    constructor(x,y,width,height){
+        /*var op={
             isStatic: true,
             density:0.2
-        }
-        this.body=Bodies.circle(x,y,radius/2,op);
-        this.radius= radius/2;
-        this.image=loadImage("walk/walking_1.png", "walk/walking_2.png", "walk/walking_3.png", "walk/walking_4.png", "walk/walking_5.png", "walk/walking_6.png", "walk/walking_7.png", "walk/walking_8.png")
-        this.image.scale=0.5;
+        } */
+        this.body=Bodies.rectangle(x,y,width,height);
+     // this.radius= radius/2;
+        this.width=width;
+        this.height=height
+        this.image=boyImg;
         World.add(world,this.body);
     }
 
@@ -16,7 +17,7 @@ class Umbrella{
         translate(this.body.position.x, this.body.position.y ); 
         rotate(this.body.angle);
         imageMode(CENTER);
-        image(this.image,0,0,this.radius);
+        image(this.image,0,0,this.width,this.height);
         pop();
     }
 }
