29d0879b27ee8d38e801e348162008e47ea9285c
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2021-01-21 17:24:11 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,17 +6,19 @@ const Constraint = Matter.Constraint;
 var drops, boy, boyImg;
 var maxDrops=100;
 
+drop=[];
+
 function preload(){
 
     boyImg=loadImage("walk/walking_1.png");
 
-   /* boyImg=loadImage("walk/walking_2.png");
+    boyImg=loadImage("walk/walking_2.png");
     boyImg=loadImage("walk/walking_3.png");
     boyImg=loadImage("walk/walking_4.png");
     boyImg=loadImage("walk/walking_5.png");
     boyImg=loadImage("walk/walking_6.png");
     boyImg=loadImage("walk/walking_7.png");
-    boyImg=loadImage("walk/walking_8.png");  */
+    boyImg=loadImage("walk/walking_8.png");  
     
 }
 
@@ -25,8 +27,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-   // drops=new Drop(250,690,20,20);
-    boy=new Umbrella(250,500,60,60);
+    drops=new Drop(250,690,20,20);
+    boy=new Umbrella(250,620,150,210);
 
 
    
@@ -34,9 +36,17 @@ function setup(){
 }
 
 function draw(){
-    background(250);
+    background(0);
     Engine.update(engine);
 
     boy.display();
-}   
+    drops.display();
 
+    for(var s=0;s<maxDrops;s++){
+        drop.push(new Drop(random(0,600),random(0,600)));
+    }
+
+    if(drops.body.position.y>height){
+        Matter.Body.setPosition(drops.body,{x:random(0,600),y:random(0,600)});
+    }
+}   
