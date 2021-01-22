764622b12b0ea17f303301f6e17bca33f67260f3
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2021-01-22 19:30:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var drops, boy, boyImg;
+var drops, boy, thunder, boyImg, thunder1Img, thunder2Img, thunder3Img, thunder4Img;
 var maxDrops=100;
 
 drop=[];
@@ -11,7 +11,6 @@ drop=[];
 function preload(){
 
     boyImg=loadImage("walk/walking_1.png");
-
     boyImg=loadImage("walk/walking_2.png");
     boyImg=loadImage("walk/walking_3.png");
     boyImg=loadImage("walk/walking_4.png");
@@ -19,19 +18,24 @@ function preload(){
     boyImg=loadImage("walk/walking_6.png");
     boyImg=loadImage("walk/walking_7.png");
     boyImg=loadImage("walk/walking_8.png");  
+
+    thunder1Img=loadImage("thunder/1.png");
+    thunder2Img=loadImage("thunder/2.png");
+    thunder3Img=loadImage("thunder/3.png");
+    thunder4Img=loadImage("thunder/4.png");
     
 }
 
 function setup(){
-    var canvas=createCanvas(500,700);
+    var canvas = createCanvas(500,700);
     engine = Engine.create();
-    world = engine.world;
+    world  = engine.world;
 
-    drops=new Drop(250,690,20,20);
-    boy=new Umbrella(250,620,150,210);
+    boy=new Umbrella(250,600,110,210);
 
-
-   
+    for(var s=0;s<maxDrops;s++){
+        drop.push(new Drop(random(0,400),random(0,400)));
+    }
     
 }
 
@@ -40,13 +44,34 @@ function draw(){
     Engine.update(engine);
 
     boy.display();
-    drops.display();
+   
+    for(var i=0;i<maxDrops;i++){
+      drop[i].display();
+      drop[i].fall();
+    } 
 
-    for(var s=0;s<maxDrops;s++){
-        drop.push(new Drop(random(0,600),random(0,600)));
-    }
-
-    if(drops.body.position.y>height){
-        Matter.Body.setPosition(drops.body,{x:random(0,600),y:random(0,600)});
-    }
+    spawnthunder();
 }   
+
+function spawnthunder() {
+    if(frameCount % 60 === 0) {
+      var thunder = createSprite(50,200,20,20);
+      thunder.x = random(50,600);
+      
+      //generate random thunder
+      var s = Math.round(random(1,4));
+      switch(s) {
+        case 1: thunder.addImage(thunder1Img);
+                break;
+        case 2: thunder.addImage(thunder2Img);
+                break;
+        case 3: thunder.addImage(thunder3Img);
+                break;
+        case 4: thunder.addImage(thunder4Img);
+                break;
+        default: break;
+      }
+      
+      thunder.scale = 0.7;
+    }
+  }
